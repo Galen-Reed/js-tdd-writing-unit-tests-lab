b5c117b9e79e8803849b902c2e266e14849d5e9b
GalenReed <galenreed@vt.edu>
2024-11-10 13:33:16 -0500

    I made this easier

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,15 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+    it("returns true when thje word is the same forwards and backwards", () => {
+        const word = "racecar";
+        const result = isPalindrome(word);
+        expect(result).toBe(true);
+    });
+    it("returns false when the word isn't the same forwards and backwards", () => {
+        const word= "car";
+        const result = isPalindrome(word);
+        expect(result).toBe(false);
+    });
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
 // Your code here
+export function isPalindrome(word) {
+    const normalizedWord = word.toLowerCase();
+    const reversedWord = normalizedWord.split('').reverse().join('');
+    return normalizedWord === reversedWord;
+}
